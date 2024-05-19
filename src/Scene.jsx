58edf9de3cd402824d540a8bd40eca85630a20b1
yungshun317 <seraph317@gmail.com>
2024-05-19 14:38:35 +0800

    [FEAT] Perform high quality text rendering with signed distance fields and anti-aliasing using `Text`

--- src/Scene.jsx
+++ src/Scene.jsx
@@ -1,6 +1,18 @@
+import {OrbitControls, Text} from "@react-three/drei";
+
 const Scene = () => {
     return (
         <>
+            <OrbitControls/>
+
+            <Text>
+                This is a Text.
+            </Text>
+
+            <mesh position-x={-1}>
+                <boxGeometry/>
+                <meshBasicMaterial color="purple"/>
+            </mesh>
         </>
     );
 };
