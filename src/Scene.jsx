aaa362f365bf35d5e53a54fe2a90766b8549ce82
yungshun317 <seraph317@gmail.com>
2024-05-19 14:48:37 +0800

    [FEAT] Use a custom font file

--- src/Scene.jsx
+++ src/Scene.jsx
@@ -3,9 +3,17 @@ import {OrbitControls, Text} from "@react-three/drei";
 const Scene = () => {
     return (
         <>
-            <OrbitControls/>
+            <OrbitControls />
 
-            <Text>
+            <Text
+                fontSize={0.4}
+                color="orange"
+                font="./static/fonts/1.ttf"
+                position-y={1.5}
+                rotation-y={Math.PI * 0.1}
+                maxWidth={2}
+                textAlign="center"
+            >
                 This is a Text.
             </Text>
 
